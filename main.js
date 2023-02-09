8fa2580d7d894ca877800770f18a676730b167af
swazkit <swastikraychoudhuri@gmail.com>
2023-02-10 00:36:04 +0530

    made it responsive and restartable

--- main.js
+++ main.js
@@ -10,7 +10,22 @@ const mouse = {
   y: innerHeight / 2
 }
 
+var n = innerWidth > 700? 400 : 200
+
+// Utility Functions
+  var randomIntFromRange = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+  var randomColor = () => {
+    return colors[Math.floor(Math.random() * colors.length)]
+  }
+
+
 const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
+
+var gravity = 1
+var fraction = 0.59
 
 // Event Listeners
 addEventListener('mousemove', (event) => {
@@ -25,35 +40,62 @@ addEventListener('resize', () => {
   init()
 })
 
+addEventListener('click', () => {
+  init()
+})
+
+
+
 // Objects
-class Object {
-  constructor(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
+class Ball {
+  constructor(x, y, radius, color, dx, dy) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    this.color = color;
   }
 
   draw() {
     c.beginPath()
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
     c.fillStyle = this.color
+    c.stroke()
     c.fill()
     c.closePath()
   }
 
   update() {
+    if(this.y + this.radius + this.dy > canvas.height){
+      this.dy = -this.dy * fraction;
+    } else {
+      this.dy += gravity;
+    }
+
+    if(this.x + this.radius > canvas.width || this.x + this.radius < 0) {
+      this.dx = -this.dx;
+    }
+
+    this.x += this.dx;
+    this.y += this.dy;
     this.draw()
   }
 }
 
+    console.log(n);
 // Implementation
-let objects
+let ballArray
+let ball = new Ball(mouse.x, mouse.y, 30, colors[1], 5)
 function init() {
-  objects = []
+  ballArray = []
 
-  for (let i = 0; i < 400; i++) {
-    // objects.push()
+  for (let i = 0; i < n; i++) {
+    var radius = randomIntFromRange(10, 30)
+    var x = randomIntFromRange(radius , canvas.width - radius)
+    var y = randomIntFromRange(0, canvas.height - radius)
+    var dx = randomIntFromRange(-2, 2)
+    ballArray.push(new Ball(x, y, radius, randomColor(), dx, 5))
   }
 }
 
@@ -61,12 +103,13 @@ function init() {
 function animate() {
   requestAnimationFrame(animate)
   c.clearRect(0, 0, canvas.width, canvas.height)
+  ball.update()
 
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  // objects.forEach(object => {
-  //  object.update()
-  // })
+  ballArray.forEach(ball => {
+    ball.update()
+  })
 }
+
 
 init()
 animate()
